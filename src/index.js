51417c906069a73a58ec8f3b7e3935b78093a9cb
Alan Seto <alantseto@gmail.com>
2017-04-03 21:18:40 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return(
+      <h1>China Chef</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
