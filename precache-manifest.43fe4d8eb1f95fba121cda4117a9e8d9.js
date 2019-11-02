568ab2312a65263d3e66027e746322606bfaeb1a
Alan Seto <alantseto@gmail.com>
2019-11-02 17:11:03 -0500

    Updates

---
+++ precache-manifest.43fe4d8eb1f95fba121cda4117a9e8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a0ccdc1a8960b306951ce2f5b36b35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3708c172ce5d40ba9178",
+    "url": "/static/css/main.0bf8aeb9.chunk.css"
+  },
+  {
+    "revision": "96b9e689425fd058f75f",
+    "url": "/static/js/2.e3564ab0.chunk.js"
+  },
+  {
+    "revision": "3708c172ce5d40ba9178",
+    "url": "/static/js/main.1226b522.chunk.js"
+  },
+  {
+    "revision": "758c892529013a572b72",
+    "url": "/static/js/runtime-main.c1d0240d.js"
+  },
+  {
+    "revision": "f40a7707a13330e81c8f6bdc2bc18d1c",
+    "url": "/static/media/china-one.f40a7707.ttf"
+  }
+]);
