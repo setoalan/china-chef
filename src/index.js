6181c595c698b82d7ea5461b92c7610c42be37c4
Alan Seto <alantseto@gmail.com>
2021-02-02 19:55:42 -0600

    Remove redux and several pages

--- src/index.js
+++ src/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/header';
-import Menu from './components/menu';
-import Catering from './components/catering';
 import Home from './components/home';
 import Footer from './components/footer';
 
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/menu" component={Menu} />
-          <Route path="/catering" component={Catering} />
-          <Route path="/" component={Home} />
-        </Switch>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+      <Footer />
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import menuReducer from './reducer_menu';
-
-const rootReducer = combineReducers({
-  menu: menuReducer
-});
-
-export default rootReducer;
